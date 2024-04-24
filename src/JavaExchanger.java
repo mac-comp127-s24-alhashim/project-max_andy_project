2f84db6cf05ef2bb842ba58c56665f03e036b312
maxchang8 <156487174+maxchang8@users.noreply.github.com>
2024-04-23 21:37:08 -0500

    Update JavaExchanger.java

--- src/JavaExchanger.java
+++ src/JavaExchanger.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -5,24 +7,38 @@ import org.jsoup.select.Elements;
 
 public class JavaExchanger {
     public static void main(String[] args) {
+        String url = "https://www.oanda.com/currency-converter/en/?from=USD&to=KRW&amount=1";
+
+
         try {
-            Document doc = Jsoup.connect("https://www.oanda.com/currency-converter/en/?from=USD&to=KRW&amount=1").get();
+            Document document = Jsoup.connect(url).get();
+            String exRate = document.select(".MuiInputBase-input.MuiFilledInput-input").text();
 
-            Elements divElements = doc.select("div.MuiInputBase-root.MuiFilledInput-root.MuiFilledInput-underline.MuiInputBase-fullWidth.MuiInputBase-formControl");
-
-            if (divElements.size() > 1) {
-                Element usdToKrwBox = divElements.get(1);
-                String usdToKrw = usdToKrwBox.text();
-
-                usdToKrw = usdToKrw.substring(272, usdToKrw.length() - 8);
-
-                System.out.println(usdToKrw);
-            } else {
-                System.out.println("Element not found");
-            }
-
-        } catch (Exception e) {
+        }catch(IOException e){
             e.printStackTrace();
         }
+
+
+
+
+        // try {
+        //     Document doc = Jsoup.connect("https://www.oanda.com/currency-converter/en/?from=USD&to=KRW&amount=1").get();
+
+        //     Elements divElements = doc.select("div.MuiInputBase-root.MuiFilledInput-root.MuiFilledInput-underline.MuiInputBase-fullWidth.MuiInputBase-formControl");
+
+        //     if (divElements.size() > 1) {
+        //         Element usdToKrwBox = divElements.get(1);
+        //         String usdToKrw = usdToKrwBox.text();
+
+        //         usdToKrw = usdToKrw.substring(272, usdToKrw.length() - 8);
+
+        //         System.out.println(usdToKrw);
+        //     } else {
+        //         System.out.println("Element not found");
+        //     }
+
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
     }
 }
